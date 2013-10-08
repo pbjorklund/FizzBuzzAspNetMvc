6e754cc2626f79547dc6989319cd3283a54ab9c4
Patrik Björklund <p.bjorklund@gmail.com>
2013-10-08 21:18:22 +0200

    Add FizzBuzz with tests

---
+++ FizzBuzz/Controllers/FizzBuzzController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzz.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        //
+        // GET: /FizzBuzz/
+        public ActionResult Index(int value)
+        {
+            ViewBag.Output = String.Concat(Enumerable.Range(1, value).Select(i => (i % 5 == 0 && i % 3 == 0) ? "FizzBuzz " : i % 5 == 0 ? "Buzz " : i % 3 == 0 ? "Fizz " : String.Format("{0} ", i))).TrimEnd();
+
+            return View();
+        }
+	}
+}
